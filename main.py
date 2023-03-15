4405f9dd9d65013cdc151fb79f480c64558f7d95
Hakimov Bulat <bulathakimov1704@gmail.com>
2023-03-15 17:15:10 +0300

    Двойная защита

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -42,6 +44,18 @@ def answer():
 def distribution():
     ausrtonauts = ['Стивен Спилберг', "Джордж Лукас", "Ридли Скотт", "Кристофер Нолан", "Дени Вильнёв"]
     return render_template('distribution.html', ausrtonauts=ausrtonauts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Доступ разрешён')
 
 
 if __name__ == '__main__':
